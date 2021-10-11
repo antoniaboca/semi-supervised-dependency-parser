682905036c31e8c967fdd07d9c42214d9332ff33
Antonia Boca <aib36@cam.ac.uk>
2021-10-11 19:41:53 +0100

    create a vocabulary of words

--- dependency-parsers/data-process.py
+++ dependency-parsers/data-process.py
@@ -1,11 +1,24 @@
 import pyconll 
 import pyconll.util
+from collections import OrderedDict
 
 UD_ENGLISH_GUM = 'dependency-parsers/en_gum-ud-train.conllu'
 
 data_load = pyconll.load_from_file(UD_ENGLISH_GUM)
 
-first_sentence = data_load[0]
-for token in first_sentence:
-    print(token.lemma)
+counter = {}
+for sentence in data_load:
+    for token in sentence:
+        if token.lemma in counter:
+            counter[token.lemma] += 1
+        else:
+            counter[token.lemma] = 1
+
+vocab = sorted([(k, v) for k, v in counter.items()], key=lambda t:t[1])
+vocab.reverse()
+
+vocab_dict = OrderedDict(vocab)
+
+for k, v in vocab_dict.items():
+    print("{}: {}".format(k,v))
 
