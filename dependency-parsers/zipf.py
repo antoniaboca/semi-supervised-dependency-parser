38464ccb5298937b1135fabcfe1dd108d7ed08c5
Antonia Boca <aib36@cam.ac.uk>
2021-10-16 14:36:45 +0100

    aplot the zipf law

---
+++ dependency-parsers/zipf.py
@@ -0,0 +1,31 @@
+from data_process import DataProcessor
+import matplotlib.pyplot as plt
+import math
+from scipy.optimize import curve_fit
+import numpy as np
+
+UD_ENGLISH_GUM = 'dependency-parsers/en_gum-ud-train.conllu'
+
+def zipf(x, k, a):
+    return k / (x**a)
+
+def plot_zipf(file):
+    data_processor = DataProcessor(file)
+    vocab = data_processor.vocab
+    words = data_processor.words
+    index_to_token = vocab.get_index_to_token_vocabulary(namespace='words')
+    tuples = [(key, value) for key, value in index_to_token.items()]
+    tuples.sort()
+
+    ys = [words[value] for _, value in tuples if value in words]
+    xs = [key for key, value in tuples if value in words]
+
+    popt, _ = curve_fit(zipf, xs, ys)
+
+    k, a = popt
+    curve = zipf(xs, k, a)
+    plt.plot(np.log2(xs), np.log2(ys), 'ro')
+    plt.plot(np.log2(xs), np.log2(curve))
+    plt.show()
+
+plot_zipf(UD_ENGLISH_GUM)
