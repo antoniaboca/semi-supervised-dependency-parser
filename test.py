66833ba0697286e9a19164da45f9c5bf08c9a3ff
Antonia Boca <aib36@cam.ac.uk>
2022-01-28 15:10:21 +0000

    add tests for feature vector

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import unittest
 from tests.data_load import DatasetTestCase, PadderTestCase
 from tests.nn_modules import BiaffineTestCase
+from tests.edge_features import PriorTestCase, Top20TestCase, FeatureVectorTestCase
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTests(unittest.makeSuite(DatasetTestCase))
     suite.addTests(unittest.makeSuite(PadderTestCase))
     suite.addTests(unittest.makeSuite(BiaffineTestCase))
+    suite.addTests(unittest.makeSuite(PriorTestCase))
+    suite.addTests(unittest.makeSuite(Top20TestCase))
+    suite.addTests(unittest.makeSuite(FeatureVectorTestCase))
     return suite
 
 if __name__ == '__main__':
